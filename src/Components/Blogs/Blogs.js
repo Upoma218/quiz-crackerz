de26a05c121ffe6b1680e4584321c1b51ddda10e
Resma Islam Upoma <upoma.cdsr@gmail.com>
2022-10-12 20:34:20 +0600

    Blog page added

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
+        <div className='p-5 m-5'>
             <h1> Blogs</h1>
             <div>
                 <h4>1. What is purpose of react router?</h4>
-                <p></p>
+                <p>React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL</p>
             </div>
             <div>
-                <h4>2. How does cotext API works?</h4>
-                <p></p>
+                <h4>2. How does context API works?</h4>
+                <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
             </div>
             <div>
                 <h4>What is "useRef"?</h4>
-                <p></p>
+                <p>Essentially, useRef is like a “box” that can hold a mutable value in its .current property. You might be familiar with refs primarily as a way to access the DOM. If you pass a ref object to React with myRef , React will set its .current property to the corresponding DOM node whenever that node changes.</p>
             </div>
         </div>
     );
--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,11 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
-import './Quiz.css'
+import './Quiz.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Quiz = ({questionData}) => {
     console.log(questionData)
     const {correctAnswer,id, options,question} = questionData;
+    const showAnswer = () => {toast(correctAnswer)}
     return (
         <div className=' quiz p-4 m-6 border border-2 rounded'>
             <h2 className='text-success'>{question}</h2>
@@ -13,9 +16,10 @@ const Quiz = ({questionData}) => {
                 {
                     options.map(option => <div><input type="radio" /><span className='ms-3'>{option}</span></div>)
                 }
-                 <span><FontAwesomeIcon className='mt-4 font-lg' icon={faEye} /></span>
+                 <span><FontAwesomeIcon onClick={() => showAnswer()} className='mt-4 font-lg' icon={faEye} /></span>
+
             </div>
-           
+           <ToastContainer/>
         </div>
     );
 };
