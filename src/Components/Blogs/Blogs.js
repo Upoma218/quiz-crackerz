c3d4b7b6db5945f675cdeb4ab2719427beba5218
Resma Islam Upoma <upoma.cdsr@gmail.com>
2022-10-12 20:04:34 +0600

    error handle file added added

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -3,7 +3,19 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-           <h1> this is blog</h1>
+            <h1> Blogs</h1>
+            <div>
+                <h4>1. What is purpose of react router?</h4>
+                <p></p>
+            </div>
+            <div>
+                <h4>2. How does cotext API works?</h4>
+                <p></p>
+            </div>
+            <div>
+                <h4>What is "useRef"?</h4>
+                <p></p>
+            </div>
         </div>
     );
 };
